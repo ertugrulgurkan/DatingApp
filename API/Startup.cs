298cb3ae528e6146dbb67a0f94570e823dff9c5f
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2021-01-23 17:21:57 +0300

    angular cli was added.

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,7 @@ namespace API
         {
 
             services.AddControllers();
+            services.AddCors();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
